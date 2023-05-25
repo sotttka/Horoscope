91e95a01763b3fa4bf40b4ac640346760aade94a
sotttka <karinattss@yandex.ru>
2023-05-25 11:14:05 +0300

    horoscope

---
+++ Horoscope/Form2.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Horoscope
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            _labelHoroscopeInfo = new MaterialSkin.Controls.MaterialLabel();
+            materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton5 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton6 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton7 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton8 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton9 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton10 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton11 = new MaterialSkin.Controls.MaterialFlatButton();
+            materialFlatButton12 = new MaterialSkin.Controls.MaterialFlatButton();
+            SuspendLayout();
+            // 
+            // _labelHoroscopeInfo
+            // 
+            _labelHoroscopeInfo.BackColor = Color.Transparent;
+            _labelHoroscopeInfo.Depth = 0;
+            _labelHoroscopeInfo.FlatStyle = FlatStyle.Popup;
+            _labelHoroscopeInfo.Font = new Font("Roboto", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            _labelHoroscopeInfo.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            _labelHoroscopeInfo.Location = new Point(162, 23);
+            _labelHoroscopeInfo.MouseState = MaterialSkin.MouseState.HOVER;
+            _labelHoroscopeInfo.Name = "_labelHoroscopeInfo";
+            _labelHoroscopeInfo.Size = new Size(752, 547);
+            _labelHoroscopeInfo.TabIndex = 0;
+            // 
+            // materialFlatButton1
+            // 
+            materialFlatButton1.AutoSize = true;
+            materialFlatButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton1.Depth = 0;
+            materialFlatButton1.Icon = null;
+            materialFlatButton1.Location = new Point(13, 255);
+            materialFlatButton1.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton1.Name = "materialFlatButton1";
+            materialFlatButton1.Primary = false;
+            materialFlatButton1.Size = new Size(57, 36);
+            materialFlatButton1.TabIndex = 1;
+            materialFlatButton1.Text = "Овен";
+            materialFlatButton1.UseVisualStyleBackColor = true;
+            materialFlatButton1.Click += _btnAries_Click;
+            // 
+            // materialFlatButton2
+            // 
+            materialFlatButton2.AutoSize = true;
+            materialFlatButton2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton2.Depth = 0;
+            materialFlatButton2.Icon = null;
+            materialFlatButton2.Location = new Point(13, 303);
+            materialFlatButton2.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton2.Name = "materialFlatButton2";
+            materialFlatButton2.Primary = false;
+            materialFlatButton2.Size = new Size(47, 36);
+            materialFlatButton2.TabIndex = 2;
+            materialFlatButton2.Text = "Лев";
+            materialFlatButton2.UseVisualStyleBackColor = true;
+            materialFlatButton2.Click += _btnLeo_Click;
+            // 
+            // materialFlatButton3
+            // 
+            materialFlatButton3.AutoSize = true;
+            materialFlatButton3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton3.Depth = 0;
+            materialFlatButton3.Icon = null;
+            materialFlatButton3.Location = new Point(13, 351);
+            materialFlatButton3.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton3.Name = "materialFlatButton3";
+            materialFlatButton3.Primary = false;
+            materialFlatButton3.Size = new Size(83, 36);
+            materialFlatButton3.TabIndex = 3;
+            materialFlatButton3.Text = "Стрелец";
+            materialFlatButton3.UseVisualStyleBackColor = true;
+            materialFlatButton3.Click += _btnSagittarius_Click;
+            // 
+            // materialFlatButton4
+            // 
+            materialFlatButton4.AutoSize = true;
+            materialFlatButton4.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton4.Depth = 0;
+            materialFlatButton4.Icon = null;
+            materialFlatButton4.Location = new Point(13, 399);
+            materialFlatButton4.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton4.Name = "materialFlatButton4";
+            materialFlatButton4.Primary = false;
+            materialFlatButton4.Size = new Size(65, 36);
+            materialFlatButton4.TabIndex = 4;
+            materialFlatButton4.Text = "Телец";
+            materialFlatButton4.UseVisualStyleBackColor = true;
+            materialFlatButton4.Click += _btnTaurus_Click;
+            // 
+            // materialFlatButton5
+            // 
+            materialFlatButton5.AutoSize = true;
+            materialFlatButton5.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton5.Depth = 0;
+            materialFlatButton5.Icon = null;
+            materialFlatButton5.Location = new Point(13, 447);
+            materialFlatButton5.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton5.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton5.Name = "materialFlatButton5";
+            materialFlatButton5.Primary = false;
+            materialFlatButton5.Size = new Size(57, 36);
+            materialFlatButton5.TabIndex = 5;
+            materialFlatButton5.Text = "Дева";
+            materialFlatButton5.UseVisualStyleBackColor = true;
+            materialFlatButton5.Click += _btnVirgo_Click;
+            // 
+            // materialFlatButton6
+            // 
+            materialFlatButton6.AutoSize = true;
+            materialFlatButton6.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton6.Depth = 0;
+            materialFlatButton6.Icon = null;
+            materialFlatButton6.Location = new Point(13, 495);
+            materialFlatButton6.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton6.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton6.Name = "materialFlatButton6";
+            materialFlatButton6.Primary = false;
+            materialFlatButton6.Size = new Size(81, 36);
+            materialFlatButton6.TabIndex = 6;
+            materialFlatButton6.Text = "Козерог";
+            materialFlatButton6.UseVisualStyleBackColor = true;
+            materialFlatButton6.Click += _btnCapricorn_Click;
+            // 
+            // materialFlatButton7
+            // 
+            materialFlatButton7.AutoSize = true;
+            materialFlatButton7.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton7.Depth = 0;
+            materialFlatButton7.Icon = null;
+            materialFlatButton7.Location = new Point(13, 543);
+            materialFlatButton7.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton7.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton7.Name = "materialFlatButton7";
+            materialFlatButton7.Primary = false;
+            materialFlatButton7.Size = new Size(97, 36);
+            materialFlatButton7.TabIndex = 7;
+            materialFlatButton7.Text = "Близнецы";
+            materialFlatButton7.UseVisualStyleBackColor = true;
+            materialFlatButton7.Click += _btnGemini_Click;
+            // 
+            // materialFlatButton8
+            // 
+            materialFlatButton8.AutoSize = true;
+            materialFlatButton8.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton8.Depth = 0;
+            materialFlatButton8.Icon = null;
+            materialFlatButton8.Location = new Point(13, 207);
+            materialFlatButton8.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton8.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton8.Name = "materialFlatButton8";
+            materialFlatButton8.Primary = false;
+            materialFlatButton8.Size = new Size(58, 36);
+            materialFlatButton8.TabIndex = 8;
+            materialFlatButton8.Text = "Весы";
+            materialFlatButton8.UseVisualStyleBackColor = true;
+            materialFlatButton8.Click += _btnLibra_Click;
+            // 
+            // materialFlatButton9
+            // 
+            materialFlatButton9.AutoSize = true;
+            materialFlatButton9.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton9.Depth = 0;
+            materialFlatButton9.Icon = null;
+            materialFlatButton9.Location = new Point(13, 159);
+            materialFlatButton9.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton9.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton9.Name = "materialFlatButton9";
+            materialFlatButton9.Primary = false;
+            materialFlatButton9.Size = new Size(86, 36);
+            materialFlatButton9.TabIndex = 9;
+            materialFlatButton9.Text = "Водолей";
+            materialFlatButton9.UseVisualStyleBackColor = true;
+            materialFlatButton9.Click += _btnAquarius_Click;
+            // 
+            // materialFlatButton10
+            // 
+            materialFlatButton10.AutoSize = true;
+            materialFlatButton10.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton10.Depth = 0;
+            materialFlatButton10.Icon = null;
+            materialFlatButton10.Location = new Point(13, 111);
+            materialFlatButton10.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton10.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton10.Name = "materialFlatButton10";
+            materialFlatButton10.Primary = false;
+            materialFlatButton10.Size = new Size(48, 36);
+            materialFlatButton10.TabIndex = 10;
+            materialFlatButton10.Text = "Рак";
+            materialFlatButton10.UseVisualStyleBackColor = true;
+            materialFlatButton10.Click += _btnCancer_Click;
+            // 
+            // materialFlatButton11
+            // 
+            materialFlatButton11.AutoSize = true;
+            materialFlatButton11.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton11.Depth = 0;
+            materialFlatButton11.Icon = null;
+            materialFlatButton11.Location = new Point(13, 63);
+            materialFlatButton11.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton11.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton11.Name = "materialFlatButton11";
+            materialFlatButton11.Primary = false;
+            materialFlatButton11.Size = new Size(96, 36);
+            materialFlatButton11.TabIndex = 11;
+            materialFlatButton11.Text = "Скорпион";
+            materialFlatButton11.UseVisualStyleBackColor = true;
+            materialFlatButton11.Click += _btnScorpio_Click;
+            // 
+            // materialFlatButton12
+            // 
+            materialFlatButton12.AutoSize = true;
+            materialFlatButton12.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialFlatButton12.Depth = 0;
+            materialFlatButton12.Icon = null;
+            materialFlatButton12.Location = new Point(13, 15);
+            materialFlatButton12.Margin = new Padding(4, 6, 4, 6);
+            materialFlatButton12.MouseState = MaterialSkin.MouseState.HOVER;
+            materialFlatButton12.Name = "materialFlatButton12";
+            materialFlatButton12.Primary = false;
+            materialFlatButton12.Size = new Size(59, 36);
+            materialFlatButton12.TabIndex = 12;
+            materialFlatButton12.Text = "Рыба";
+            materialFlatButton12.UseVisualStyleBackColor = true;
+            materialFlatButton12.Click += _btnFish_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(926, 595);
+            Controls.Add(materialFlatButton12);
+            Controls.Add(materialFlatButton11);
+            Controls.Add(materialFlatButton10);
+            Controls.Add(materialFlatButton9);
+            Controls.Add(materialFlatButton8);
+            Controls.Add(materialFlatButton7);
+            Controls.Add(materialFlatButton6);
+            Controls.Add(materialFlatButton5);
+            Controls.Add(materialFlatButton4);
+            Controls.Add(materialFlatButton3);
+            Controls.Add(materialFlatButton2);
+            Controls.Add(materialFlatButton1);
+            Controls.Add(_labelHoroscopeInfo);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel _labelHoroscopeInfo;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton5;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton6;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton7;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton8;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton9;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton10;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton11;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton12;
+    }
+}
